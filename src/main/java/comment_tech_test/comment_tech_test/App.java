32142ddb8f8c56e925b1dacf1e3fa692372669dc
Aaron Hays <aaron.hays_7@yahoo.com>
2021-02-13 17:26:30 +0000

    initialized project with maven dependencies

---
+++ src/main/java/comment_tech_test/comment_tech_test/App.java
@@ -0,0 +1,13 @@
+package comment_tech_test.comment_tech_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/comment_tech_test/comment_tech_test/AppTest.java
@@ -0,0 +1,20 @@
+package comment_tech_test.comment_tech_test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
