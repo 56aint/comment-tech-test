8e6c03facb2eb0059c6eb1b4bacbd2fcf8ba880b
Aaron Hays <aaron.hays_7@yahoo.com>
2021-02-14 07:43:04 +0000

    code refactored and formatted

--- src/test/java/pageObjects/CommentPage.java
+++ src/test/java/pageObjects/CommentPage.java
@@ -4,24 +4,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
-
 public class CommentPage {
-	public WebDriver driver;
-	public CommentPage(WebDriver ldriver) {
-		driver = ldriver;
-		PageFactory.initElements(ldriver, this);	
-	}
-	
-	
-	By txtareabox = By.xpath("//input[@class='comments-input-box']");
-	     
-	
-	public void clickTxtareabox() {
-		driver.findElements(txtareabox).size();
-	}
+  public WebDriver driver;
+
+  public CommentPage(WebDriver ldriver) {
+    driver = ldriver;
+    PageFactory.initElements(ldriver, this);
+  }
+
+  By txtareabox = By.xpath("//input[@class='comments-input-box']");
+
+  public void clickTxtareabox() {
+    driver.findElements(txtareabox).size();
+  }
 }
-
-
-
-	
-
--- src/test/java/testRunner/testRun.java
+++ src/test/java/testRunner/testRun.java
@@ -1,19 +1,16 @@
 package testRunner;
-import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = ".//Features/Comment.feature", 
-		glue = "stepDefinitions", 
-		dryRun = false, 
-		monochrome = true, 
-		plugin = { "pretty", "html:target/cucumber-report.html" },
-		publish = true
+  features = ".//Features/Comment.feature",
+  glue = {"stepDefinitions"},
+  dryRun = false,
+  monochrome = true,
+  plugin = { "pretty", "html:target/cucumber-report.html" },
+  publish = true
 )
-
-public class testRun {
-
-}
+public class testRun {}
